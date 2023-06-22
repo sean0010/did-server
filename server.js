9ecf387a88d02308fe0915b7cae59cb05b139aa6
sean0012 <waysummer@gmail.com>
2023-06-22 15:59:58 +0900

    websocket server

--- server.js
+++ server.js
@@ -1,15 +1,41 @@
-const { WebSocketServer } = require('ws');
+const { WebSocketServer, WebSocket } = require('ws');
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
+const heartbeat = () => {
+  this.isAlive = true;
+};
+
+
+wss.on('connection', function(ws) {
+  ws.isAlive = true;
+  //ws.on('pong', heartbeat);
+  const interval = setInterval(() => {
+    wss.clients.forEach((ws) => {
+      if (ws.isAlive === false) return ws.terminate();
+
+      //ws.isAlive = false;
+      //ws.ping();
+    });
+  }, 30000);
+  wss.on('close', () => {
+    clearInterval(interval);
+  });
   ws.on('error', console.error);
 
-  ws.on('message', function message(data, isBinary) {
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(data, { binary: isBinary });
-      }
-    });
+  ws.on('message', (data, isBinary) => {
+    console.log('isBinary:',isBinary);
+    console.log('get data:',data.toString());
+    try {
+      const json = JSON.parse(data.toString());
+      wss.clients.forEach(function(client) {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(data, { binary: isBinary });
+        }
+      });
+    } catch(e) {
+      console.error('OnMessage JSON exception:', e);
+      console.error('OnMessage JSON data:', data.toString());
+    }
   });
 });
